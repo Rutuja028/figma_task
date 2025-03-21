b976765f531166a8e4cc1e47b7185aa84e818b70
Rutuja <rutujassr281@gmail.com>
2025-03-21 18:33:38 +0530

    Projects page

--- lib/views/projects_page.dart
+++ lib/views/projects_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Projects extends StatefulWidget {
   const Projects({super.key});
@@ -6,10 +7,92 @@ class Projects extends StatefulWidget {
   State createState() => _ProjectsState();
 }
 
+class ProjectBottomNavigate {
+  final Widget projectBottomPage;
+  ProjectBottomNavigate(this.projectBottomPage);
+}
+
 class _ProjectsState extends State<Projects> {
+  int _selectedInd = 0;
+  void _onProjecTapped(int ind) {
+    setState(() {
+      _selectedInd = ind;
+
+      Navigator.replace(context,
+          oldRoute: ModalRoute.of(context)!,
+          newRoute: MaterialPageRoute(
+              builder: (context) => pageList[ind].projectBottomPage));
+    });
+  }
+
+  List<ProjectBottomNavigate> pageList = [
+    ProjectBottomNavigate(Container()),
+    ProjectBottomNavigate(Container()),
+    ProjectBottomNavigate(Container()),
+    ProjectBottomNavigate(Container()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        elevation: 0,
+        onDestinationSelected: _onProjecTapped,
+        selectedIndex: _selectedInd,
+        backgroundColor: Colors.white,
+        indicatorColor: Colors.white,
+        destinations: <Widget>[
+          NavigationDestination(
+            icon: SvgPicture.asset(
+              "assets/explore.svg",
+              colorFilter:
+                  const ColorFilter.mode(Color(0xFFD4D6DD), BlendMode.srcIn),
+            ),
+            selectedIcon: SvgPicture.asset(
+              "assets/explore.svg",
+              colorFilter:
+                  const ColorFilter.mode(Color(0xFF006FFD), BlendMode.srcIn),
+            ),
+            label: 'Explore',
+          ),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                "assets/projects.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFFD4D6DD), BlendMode.srcIn),
+              ),
+              selectedIcon: SvgPicture.asset(
+                "assets/projects.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFF006FFD), BlendMode.srcIn),
+              ),
+              label: 'Projects'),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                "assets/inbpx.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFFD4D6DD), BlendMode.srcIn),
+              ),
+              selectedIcon: SvgPicture.asset(
+                "assets/inbox.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFF006FFD), BlendMode.srcIn),
+              ),
+              label: 'Inbox'),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                "assets/friends.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFFD4D6DD), BlendMode.srcIn),
+              ),
+              selectedIcon: SvgPicture.asset(
+                "assets/friends.svg",
+                colorFilter:
+                    const ColorFilter.mode(Color(0xFF006FFD), BlendMode.srcIn),
+              ),
+              label: 'Profile')
+        ],
+      ),
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
